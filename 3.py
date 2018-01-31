185f5123d8bcdd276dbcee25706dfc74e4fe306b
xucoder <983299520@qq.com>
2018-01-31 12:08:23 +0800

    hannueta unfinished

---
+++ 3.py
@@ -0,0 +1,12 @@
+def move(n,a,b,c):
+    if n==1:
+        print('A-->C')
+    elsev :    
+        for i in (n-1):
+            print('A-->B')
+    print('A-->C')
+    print('B-->C')
+    return
+	
+move(4,'A','B','C')
+a=raw_input()
